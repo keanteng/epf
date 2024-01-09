f8147be6cd6aef1b16bc0446d8b502f459419f6d
keanteng <u2004763@siswa.um.edu.my>
2024-01-09 18:52:28 +0800

    beta completed

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.sidebar.title("EPF Projection Calculator")
 st.sidebar.caption("Your Goal Towards Retirement")
 
 nationality = st.sidebar.selectbox("Nationality", ["Malaysian", "Non-Malaysian"])
-salary = st.sidebar.number_input("Your Monthly Salary", value=3000, step=100, max_value=20000)
+salary = st.sidebar.number_input("Your Monthly Salary", value=3000, step=100)
 bonus = st.sidebar.number_input("Your Yearly Bonus", value=5000, step=100)
 compound_rate = st.sidebar.number_input("Your Expected Compound Rate (%)", value=6.5, step=0.5)
 goal = st.sidebar.slider("Your Saving Goal", min_value=1000000, max_value=10000000, step = 1000000)
@@ -26,13 +26,24 @@ st.sidebar.caption("GPLv3 © Kean Teng 2024")
 # main page setup
 data = get_data()
 df = get_schedule(nationality, data)
-contribution = match_contribution(df, salary)
-years = goal_seek(goal, contribution, compound_rate)
+if salary > 20000:
+    contribution = twenty_above_contribution(nationality, salary)
+else:
+    contribution = match_contribution(df, contribution_lookup(df, salary))
+if bonus > 20000:
+    bonus_contribution = twenty_above_bonus_contribution(nationality, bonus)
+else:
+    bonus_contribution = match_bonus_contribution(df, bonus_contribution_lookup(df, bonus))
+
+# goal seek
+years = goal_seek(goal, contribution, bonus_contribution, compound_rate)
 total_by_year = year_total(years, contribution, compound_rate)
 
 st.header("👋 Hi there!")
 st.write("Based on your current salary, you will need to work for ", round(years, 2), " years to achieve your saving goal.")
-st.write("Below is your contribution schedule for your reference.")
+with st.expander("📊 View Your Contribution Summary"):
+    st.dataframe(salary_contribution_summary(df, contribution_lookup(df, salary), salary), hide_index=True)
+    st.dataframe(bonus_contribution_summary(df, bonus_contribution_lookup(df, bonus), bonus), hide_index=True)
 
 # plotly
 fig = px.line(total_by_year, x=total_by_year.index, y="total", title="Your Contribution Schedule")
--- backend/functions.py
+++ backend/functions.py
@@ -1,11 +1,31 @@
+"""
+Author: keanteng
+License: GNU GPLv3
+"""
+
 import pandas as pd
 import numpy as np
+import math
 
 def get_data():
+    """Load data from excel file.
+
+    Returns:
+        df : pandas dataframe.
+    """
     df = pd.ExcelFile('data/kwsp_schedule.xlsx')
     return df
 
 def get_schedule(nationality, df):
+    """Specify the excel sheet to be used.
+
+    Args:
+        nationality (string): Malaysian or Non-Malaysian.
+        df (Dataframe): Dataframe from get_data().
+
+    Returns:
+        Dataframe: Data frame of the contribution schedule.
+    """
     if nationality == "Malaysian":
         epf_citizen = df.parse('citizen')
         return epf_citizen
@@ -13,22 +33,79 @@ def get_schedule(nationality, df):
         epf_foreign = df.parse('foreigner')
         return epf_foreign
     
-def match_contribution(df, salary):
-    temp = map(lambda x: x <= salary, df['upper'])
-    idx = list(temp).index(False)
+def contribution_lookup(df, salary):
+    """Find the contribution rate based on salary.
+
+    Args:
+        df (Dataframe): Dataframe from get_schedule().
+        salary (Numeric): Salary.
+
+    Returns:
+        idx : Index of the contribution rate.
+    """
+    try:
+        temp = map(lambda x: x <= salary, df['upper'])
+        idx = list(temp).index(False)
+    except:
+        idx = 0
+    return idx
+
+def match_contribution(df, idx):
+    """Find the contribution rate based on salary.
+
+    Args:
+        df (dataframe): Dataframe from get_schedule().
+        idx (numeric): Index of the contribution rate.
+
+    Returns:
+        contribution : Contribution rate.
+    """
     contribution = df['total'][idx]
     return contribution
 
-def goal_seek(goal, contribution, compound_rate):
+def bonus_contribution_lookup(df, bonus):
+    try:
+        temp = map(lambda x: x <= bonus, df['upper'])
+        idx = list(temp).index(False)
+    except:
+        idx = 0
+    return idx
+
+def match_bonus_contribution(df, idx):
+    bonus_contribution = df['total'][idx]
+    return bonus_contribution
+
+def goal_seek(goal, contribution, bonus_contribution, compound_rate):
+    """Find the number of years to achieve the saving goal.
+
+    Args:
+        goal (numeric): Saving goal.
+        contribution (numeric): Contribution rate per month.
+        compound_rate (numeric): Expected compound rate.
+
+    Returns:
+         years: Number of years to achieve the saving goal.
+    """
     r = 1 + compound_rate/100
+    contribution = contribution + bonus_contribution/12
     years = 1/np.log(r)*np.log((goal*(r-1))/(12*contribution*r) + 1)
     return years
 
 def year_total(years, contribution, compound_rate):
+    """Find the total contribution by year.
+
+    Args:
+        years (numeric): Number of years to achieve the saving goal.
+        contribution (numeric): Contribution rate per month.
+        compound_rate (numeric): Expected compound rate.
+
+    Returns:
+         total_by_year: A dataframe of the total contribution by year.
+    """
     r = 1 + compound_rate/100
     total = 0
     total_by_year = []
-    for i in range(1,int(years)+2):
+    for i in range(1,math.ceil(years)+2):
         temp = contribution*12*(r**i)
         total = temp + total
         total_by_year.append(total)
@@ -36,4 +113,52 @@ def year_total(years, contribution, compound_rate):
     total_by_year = pd.DataFrame(total_by_year, columns=['total'])
     
     return total_by_year
+
+def salary_contribution_summary(df, idx_salary, salary):
+    if salary <= 20000:
+        employee_contribution = df['employee'][idx_salary]
+        employer_contribution = df['employer'][idx_salary]
+        total_contribution = df['total'][idx_salary]
+    else:
+        employee_contribution = math.ceil(salary*0.11)
+        employer_contribution = math.ceil(salary*0.12)
+        total_contribution = employee_contribution + employer_contribution
     
+    summary = pd.DataFrame({'Salary': salary, 
+                            'Net Salary': salary - employee_contribution,
+                            'Employee Contribution': employee_contribution, 
+                            'Employer Contribution': employer_contribution, 
+                            'Total Contribution': total_contribution}, index=[0])
+
+    return summary
+
+def bonus_contribution_summary(df, idx_bonus, bonus):
+    if bonus <= 20000:
+        employee_contribution = df['employee'][idx_bonus]
+        employer_contribution = df['employer'][idx_bonus]
+        total_contribution = df['total'][idx_bonus]
+    else:
+        employee_contribution = math.ceil(bonus*0.11)
+        employer_contribution = 5
+        total_contribution = employee_contribution + employer_contribution
+    
+    summary = pd.DataFrame({'Bonus': bonus,
+                            'Net Bonus': bonus - employee_contribution,
+                            'Employee Contribution': employee_contribution, 
+                            'Employer Contribution': employer_contribution, 
+                            'Total Contribution': total_contribution}, index=[0])
+    return summary
+
+def twenty_above_contribution(nationality, salary):
+    if nationality == "Malaysian":
+        contribution = math.ceil(salary*0.11) + math.ceil(salary*0.12)
+    else:
+        contribution = math.ceil(salary*0.11) + 5
+    return contribution
+
+def twenty_above_bonus_contribution(nationality, bonus):
+    if nationality == "Malaysian":
+        contribution = math.ceil(bonus*0.11) + math.ceil(bonus*0.12)
+    else:
+        contribution = math.ceil(bonus*0.11) + 5
+    return contribution
